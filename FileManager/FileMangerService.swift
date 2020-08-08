0950a0042912e9d7212c160cd3205336efedee62
Valentina Rodionova <mrs.rodionova@gmail.com>
2020-08-08 12:37:29 +0300

    Initial Commit

---
+++ FileManager/FileMangerService.swift
@@ -0,0 +1,13 @@
+//
+//  FileMangerService.swift
+//  FileManager
+//
+//  Created by Rodianov on 08.08.2020.
+//  Copyright © 2020 Rodionova. All rights reserved.
+//
+
+import Foundation
+
+struct FileManagerService {
+    
+}
