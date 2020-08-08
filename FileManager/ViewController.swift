98b455c0eabf35545a36e0ddc3e663415c16ac57
Valentina Rodionova <mrs.rodionova@gmail.com>
2020-08-08 12:15:59 +0300

    Initial Commit

---
+++ FileManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FileManager
+//
+//  Created by Rodianov on 08.08.2020.
+//  Copyright © 2020 Rodionova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
